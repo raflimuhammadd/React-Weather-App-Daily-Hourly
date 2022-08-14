2c783a5b6aadb596dbab413198ed3ceb4d0ffa1e
raflimuhammadd <raflimuhh09@gmail.com>
2022-08-14 20:16:01 +0700

    ADD: FIRST

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+
+            <p className="text-white text-xl font-extralight">
+                Tuesday, 35th of May 2025 | Local Time 12;56PM
+            </p>
+        </div>
+        <div className="flex items-center justify-center my-3">
+            <p className="text-white text-3xl font-medium">Berlin, DE</p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
